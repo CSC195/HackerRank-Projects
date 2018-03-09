d1118119dd8189bad455f39fe9fc548117f377d4
CSC195 <chriscline@3alpha.us>
2018-03-08 23:46:32 -0500

    Update Day3Challenge.java

--- Day3Challenge.java
+++ Day3Challenge.java
@@ -5,7 +5,12 @@ import java.util.Scanner;
  */
 
 /**
- * @author gracecline
+ * @author CSC195
+ * Ask the user for an integer 'n' and perform the folling functions:
+ * If n is odd, print "Weird"
+ * If n is even and in the inclusive range of 2 to 5 , print Not Weird
+ * If n is even and in the inclusive range of 6 to 20, print Weird
+ * If n is even and greater than 20, print Not Weird
  *
  */
 public class Solution 
