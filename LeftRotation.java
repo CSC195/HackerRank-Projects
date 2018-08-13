a01fa45dca3f48c2199e86cd689fc98c8b55d70c
CSC195 <chriscline@3alpha.us>
2018-08-13 12:57:16 -0400

    Update LeftRotation.java

--- LeftRotation.java
+++ LeftRotation.java
@@ -1,3 +1,6 @@
+// A left rotation operation on an array shifts each of the array's elements  unit to the left. 
+//For example, if 4 left rotations are performed on array [1, 2, 3, 4, 5], then the array would become [5, 1, 2, 3, 4].
+
 import java.util.Scanner;
 
 public class LeftRotation 
